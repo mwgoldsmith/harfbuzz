4a6ce6f940fb444ed7d9a4544406f20bd99b6678
mwgoldsmith <mike.w.goldsmith@gmail.com>
2016-02-16 21:10:20 +0000

    Working msvc 2015 project files

---
+++ projects/vs14/config.h
@@ -0,0 +1,121 @@
+/* The normal alignment of `struct{char;}', in bytes. */
+#define ALIGNOF_STRUCT_CHAR__ 4
+
+/* Define to 1 if you have the `atexit' function. */
+#define HAVE_ATEXIT 1
+
+/* Have cairo graphics library */
+/* #undef HAVE_CAIRO */
+
+/* Have cairo-ft support in cairo graphics library */
+/* #undef HAVE_CAIRO_FT */
+
+/* Have Core Text backend */
+/* #undef HAVE_CORETEXT */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+/* #undef HAVE_DLFCN_H */
+
+/* Have simple TrueType Layout backend */
+#define HAVE_FALLBACK 1
+
+/* Have FreeType 2 library */
+#define HAVE_FREETYPE 1
+
+/* Define to 1 if you have the `getpagesize' function. */
+#define HAVE_GETPAGESIZE 1
+
+/* Have glib2 library */
+/* #undef HAVE_GLIB */
+
+/* Have gobject2 library */
+/* #undef HAVE_GOBJECT */
+
+/* Have Graphite2 library */
+/* #undef HAVE_GRAPHITE2 */
+
+/* Have ICU library */
+/* #undef HAVE_ICU */
+
+/* Have Intel __sync_* atomic primitives */
+#define HAVE_INTEL_ATOMIC_PRIMITIVES 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `isatty' function. */
+#define HAVE_ISATTY 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `mmap' function. */
+/* #undef HAVE_MMAP */
+
+/* Define to 1 if you have the `mprotect' function. */
+#define HAVE_MPROTECT 1
+
+/* Have native OpenType Layout backend */
+#define HAVE_OT 1
+
+/* Have POSIX threads */
+/* #undef HAVE_PTHREAD */
+
+/* Have PTHREAD_PRIO_INHERIT. */
+/* #undef HAVE_PTHREAD_PRIO_INHERIT */
+
+/* Define to 1 if you have the <sched.h> header file. */
+/* #undef HAVE_SCHED_H */
+
+/* Have sched_yield */
+/* #undef HAVE_SCHED_YIELD */
+
+/* Have Solaris __machine_*_barrier and atomic_* operations */
+/* #undef HAVE_SOLARIS_ATOMIC_OPS */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+/* #undef HAVE_STRINGS_H */
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `sysconf' function. */
+/* #undef HAVE_SYSCONF */
+
+/* Define to 1 if you have the <sys/mman.h> header file. */
+/* #undef HAVE_SYS_MMAN_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Have UCDN Unicode functions */
+#define HAVE_UCDN 1
+
+/* Have Uniscribe library */
+#define HAVE_UNISCRIBE 1
+
+/* Define to 1 if you have the <unistd.h> header file. */ 
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <usp10.h> header file. */
+/* #undef HAVE_USP10_H */
+
+/* Define to 1 if you have the <windows.h> header file. */
+#define HAVE_WINDOWS_H 1
+
+/* Define to necessary symbol if this constant
+ uses a non-standard name on
+   your system. */
+/* #undef PTHREAD_CREATE_JOINABLE */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
